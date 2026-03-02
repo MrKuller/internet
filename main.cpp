97551198c272ed8f474ed2a9e26c0e544663c1d1
MrKuller <96410316+MrKuller@users.noreply.github.com>
2026-03-02 17:54:52 +1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
 #include <Arduino.h>
 
+const int red = 3;
+const int ye = 5;
+const int green = 7;
+const int butt = 1;
+
 void setup() {
-  pinMode(10, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(ye, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(butt, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(10, HIGH);
-  delay(1000);
-  digitalWrite(10, LOW);
-  delay(1000);
+  digitalWrite(red, HIGH);
+
+  if (digitalRead(butt) == LOW){
+    delay(50);
+
+    for (int i = 0; i < 3; i++){
+      digitalWrite(ye, HIGH);
+      delay(500);
+      digitalWrite(ye, LOW);
+      delay(500);
+    }
+
+    digitalWrite(red, LOW);
+    digitalWrite(green, HIGH);
+    delay(3000);
+
+    for (int i = 0; i < 3; i++){
+      digitalWrite(ye, HIGH);
+      delay(500);
+      digitalWrite(ye, LOW);
+      delay(500);
+    }
+    digitalWrite(green, LOW);
+  }
 }
